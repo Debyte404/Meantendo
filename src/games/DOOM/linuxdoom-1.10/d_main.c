b620ea5becb2360d8b713e05984d29cb947cae7e
teerth sharma <78080953+teerthsharma@users.noreply.github.com>
2026-01-10 00:01:36 +0530

    ok

--- src/games/DOOM/linuxdoom-1.10/d_main.c
+++ src/games/DOOM/linuxdoom-1.10/d_main.c
@@ -40,6 +40,11 @@ static const char rcsid[] = "$Id: d_main.c,v 1.8 1997/02/03 22:45:09 b1 Exp $";
 #include <fcntl.h>
 #endif
 
+// Always include these for ESP32/Cross-platform support
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 
 #include "doomdef.h"
 #include "doomstat.h"
